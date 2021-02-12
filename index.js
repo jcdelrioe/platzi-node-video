9d7b2521437567e8b3a16333ea8c82e525e410a6
Juan Carlos del Rio <jcdre@hotmail.com>
2021-02-12 16:27:17 -0500

    init, connect Mongo

---
+++ index.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const app = express();
+
+const { config } = require('./config/index');
+const moviesApi = require('./routes/movies');
+
+const { logErrors, errorHandler } = require('./utils/middleware/errorhandlers');
+
+//body parser middleware
+app.use(express.json());
+
+moviesApi(app);
+
+app.use(logErrors);
+app.use(errorHandler);
+
+app.listen(config.port, function () {
+  console.log(`Listening http://localhost:${config.port}`);
+});
+
+// app.get('/', function (req, res) {
+//   res.send('Hola mundillo');
+// });
+// app.get('/json', function (req, res) {
+//   res.json({ hola: 'Mundillooo' });
+// });
+// app.get('/is_leap/:year', function (req, res) {
+//   const { year } = req.params;
+
+//   let isLeap = false;
+
+//   if (isNaN(year)) {
+//     res.send(`Year ${year} is not a number`);
+//     return;
+//   }
+
+//   const yearAsNum = parseInt(year);
+// console.log(yearAsNum);
+// if (isNaN(yearAsNum)) {
+//   res.send(`Year ${year} is not a number`);
+//   return;
+// }
+// if (yearAsNum % 4 !== 0) {
+//   isLeap = false;
+// } else if (yearAsNum % 100 !== 0) {
+//   isLeap = true;
+// } else if (yearAsNum % 400 !== 0) {
+//   isLeap = false;
+// } else {
+//   isLeap = true;
+// }
+
+//   isLeap = leapYear(yearAsNum);
+
+//   res.send(`Year ${year} is ${isLeap ? 'leap' : 'common'}`);
+// });
